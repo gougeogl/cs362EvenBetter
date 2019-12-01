73a6f877d1e0197870cb881294be9f3b6c3f952b
Glen Gougeon <gougeogl@oregonstate.edu>
2019-11-30 23:40:20 -0800

    commit 18
    bug7unittest.c
    stopping point
    NEED: test on flip3

--- FinalProject-Bugs/dominion/bug7unittest.c
+++ FinalProject-Bugs/dominion/bug7unittest.c
@@ -46,7 +46,7 @@ int main()
 	/* ** SETUP TEST SECTION ** */
 	/* MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM */
 
-	int kingdomCards[10] = { adventurer, ambassador, baron, estate, tribute, minion, mine,  gardens, remodel, smithy };
+	int kingdomCards[10] = { adventurer, ambassador, baron, estate, tribute, minion, mine, gardens, remodel, smithy };
 
 	struct gameState G;
 	struct gameState backup;
@@ -118,25 +118,6 @@ int main()
 
 	/*Assert that the top 2 cards from the next player's previous deck
 	are exactly the ones found in their discard (playedCards) after the use of tribute.*/
-	/*if (backup.deck[nextPlayer][backup.deckCount[nextPlayer] - 1] != G.playedCards[G.playedCardCount - 1])
-	{
-		if (backup.deck[nextPlayer][backup.deckCount[nextPlayer] - 2] != G.playedCards[G.playedCardCount - 2])
-		{			
-			memset(name, '\0', sizeof name);
-			cardNumToName(G.playedCards[G.playedCards[G.playedCardCount - 1], name);
-		
-			memset(nombre, '\0', sizeof name);
-			cardNumToName(G.playedCards[G.playedCards[G.playedCardCount - 2], nombre);
-
-			printf("Tribute Error: previous top 2 deck cards next player..\n");
-			printf("               not found in next player's discard.\n");
-			printf("               Found Location: playedCards\n");
-			printf("  G.playedCards[%d]: %s\n", G.playedCardCount - 1, name);
-			printf("  G.playedCards[%d]: %s\n", G.playedCardCount - 2, nombre);
-
-		}
-	}*/
-
 	if (backup.deck[nextPlayer][backup.deckCount[nextPlayer] - 1] != G.discard[nextPlayer][G.discardCount[nextPlayer] - 1])
 	{
 		if (backup.deck[nextPlayer][backup.deckCount[nextPlayer] - 2] != G.discard[nextPlayer][G.discardCount[nextPlayer] - 2])
@@ -158,19 +139,143 @@ int main()
 	/*Assert that the counts of the next player's discard and deck counts
 	are correct, such that their deckCount is 2 less than previously, and
 	their discardCount is 2 greater than previously.*/
+	if (backup.deckCount[nextPlayer] - 2 != G.deckCount[nextPlayer])
+	{
+		if (backup.discardCount[nextPlayer] + 2 != G.discardCount[nextPlayer])
+		{
+			printf("Tribute Error: next player's deckCount not -2 previous.\n");
+			printf("             : next player's discardCount not +2 previous.\n");
+
+		}
+	}	  
 
 	/*Assert if one of the top 2 cards in the next player's discard
-	are a coin, that the current player's coin is exactly 2 greater than previously.*/
+	are a coin, that the current player's coin is exactly 2 greater than previously.
+	for each treasure in discard*/
+	int totalCoin = 0;
+	if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == copper ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == silver ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == gold) {
+
+		// 1 treasure found
+		totalCoin += 2;
+
+			if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 2] == copper ||
+				G.discard[nextPlayer][G.discardCount[nextPlayer] - 2] == silver ||
+				G.discard[nextPlayer][G.discardCount[nextPlayer] - 2] == gold){
+
+				// 2nd treasure found
+				totalCoin += 2;
+			}
+	}
+
+	if (totalCoin == 2)
+	{
+		if (backup.coins + 2 != G.coins)
+		{
+			printf("Tribute Error: You had 1 treasure in discard, but didn't gain 2 coin.\n");
+		}
+	}
+	else if (totalCoin == 4)
+	{
+		if (backup.coins + 4 != G.coins)
+		{
+			printf("Tribute Error: You had 2 treasures in discard, but didn't gain 4 coin.\n");
+		}
+	}
+	/*Assert that if the current player's coin went up by 2 from previous, that
+	the next player's top 2 discarded cards are a coin, otherwise print a message*/
+	else if (totalCoin == 0 && (backup.coins != G.coins))
+	{
+		printf("Tribute Error: You gained coin, but didn't find a treasure in discard.\n");
+	}
 
 	/*Assert if one of the top 2 cards in the next player's discard
 	are a victory card, that the current player's handCount is 2 greater than previously.*/
+	int totalVictory = 0;
+	if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == estate ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == gardens) {
 
-	/*Assert that if the current player's coin went up by 2 from previous, that 
-	the next player's top 2 discarded cards are a coin, otherwise print a message*/
+		// 1 treasure found
+		totalVictory += 2;
+
+		if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 2] == estate ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 2] == gardens) {
+
+			// 2nd treasure found
+			totalVictory += 2;
+		}
+	}
+
+	if (totalVictory == 2)
+	{
+		if (backup.handCount[nextPlayer] + 2 != G.handCount[nextPlayer])
+		{
+			printf("Tribute Error: You had 1 Victory card in discard, but didn't gain 2 card.\n");
+		}
+	}
+	else if (totalVictory == 4)
+	{
+		if (backup.handCount[nextPlayer] + 4 != G.handCount[nextPlayer])
+		{
+			printf("Tribute Error: You had 2 Victory card in discard, but didn't gain 4 cards.\n");
+		}
+	}
+	/*Assert that if the current player's hand count went up by 2 from previous, that
+	the next player's top 2 discarded cards are a victory card.*/
+	else if (totalVictory == 0 && (backup.handCount[nextPlayer]!= G.handCount[nextPlayer]))
+	{
+		printf("Tribute Error: You gained cards in hand, but didn't find a Victory card in discard.\n");
+	}
 
 	// This will catch the error.
 	/*Assert that if the current player's numActions went up by 2 from previous, that 
 	the next player's top 2 discarded cards are an action card, otherwise print a message*/
+
+	adventurer, ambassador, baron, estate, tribute, minion, mine, gardens, remodel, smithy
+	int totalActions = 0;
+	if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == adventurer ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == baron ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == tribute ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == minion ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == mine ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == remodel ||
+		G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == smithy ){
+
+		// 1 action found
+		totalActions += 2;
+
+		if (G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == adventurer ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == baron ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == tribute ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == minion ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == mine ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == remodel ||
+			G.discard[nextPlayer][G.discardCount[nextPlayer] - 1] == smithy) {
+
+			// 2nd actions found
+			totalActions += 4;
+		}
+	}
+
+	if (totalActions == 2)
+	{
+		if (backup.numActions + 2 != G.numActions)
+		{
+			printf("Tribute Error: You had 1 action card in discard, but didn't gain 2 actions.\n");
+		}
+	}
+	else if (totalActions == 4)
+	{
+		if (backup.numActions + 4 != G.numActions)
+		{
+			printf("Tribute Error: You had 2 action cards in discard, but didn't gain 4 actions.\n");
+		}
+	}
+	else if (totalActions == 0 && (backup.numActions != G.numActions))
+	{
+		printf("Tribute Error: You gained actions, but didn't find any action cards in discard.\n");
+	}
 
 	/*Assert that if the current player's hand count went up by 2 from previous, that 
 	the next player's top 2 discarded cards are a victory card.

a2f59096dbb663fa868a5974ec70abe1a57799aa
reuben <rjyoungblom@gmail.com>
2019-12-02 21:33:17 -0800

    refactored unit tests youngblr

--- FinalProject-Bugs/dominion/bug6unittest.c
+++ FinalProject-Bugs/dominion/bug6unittest.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 
-
+/*
 void assertLess (int coinStorage, int currentCoin) {
 
     // Assertion
@@ -22,6 +22,7 @@ void assertLess (int coinStorage, int currentCoin) {
         printf("Coins are incorrectly calculated for current player when buying card worth less than 5. Error detected -- player had too few coins.\n\n");
     }
 }
+
 
 void assertMore (int costs, int newCost) {
 
@@ -38,7 +39,7 @@ void assertMore (int costs, int newCost) {
         printf("Player was able to buy card worth more than 5. Error detected.\n");
     }
 }
-
+*/
 
 
 int main () {
@@ -47,6 +48,22 @@ printf("/****************************************\n");
 printf("/***********   BUG 6 TEST   *************\n");
 printf("/****************************************\n");
 
+printf("\n");
+printf("I actually spent a lot of time debugging this section before concluding that there is, in fact, no bug.\n");
+printf("The full write-up can be found in Part A, but a synopsis is below:\n");
+
+printf("\n");
+
+printf("The bug thread states that the updateCoins function is causing a bug by simply giving the player 5 coins.\n");
+printf("The argument is that this allows the player to spend more than 5 coins or keep the extras that are unspent.\n");
+printf("However, this is based on a misunderstanding of updateCoins(). This function doesn't simply give coins.\n");
+printf("Rather, it resets the coin count, and then gives fresh coins according to the player's hand and any passed-in bonus.\n");
+printf("In Feast, the hand is set to -1, and then 5 is passed into the updateCoins as a bonus, effectively resetting the coin count to 5, regardless of a player's treasure cards or existing coins.\n");
+printf("Then, the hand is restored, and updateCoins is called again with a bonus of 0, clearing any leftover coins from Feast.\n");
+printf("Together, these negate both of the bugs listed in the bug thread. There is no bug with updateCoins in Feast.\n");
+
+
+/*
 int currentPlayer = 0;
 
 struct gameState G;
@@ -66,10 +83,12 @@ G.supplyCount[6] = 3;
 
 G.hand[currentPlayer][0] = feast;
 G.hand[currentPlayer][1] = feast;
+G.hand[currentPlayer][2] = gold;
 
 updateCoins(currentPlayer, &G, 0);
 
 int coinStorage = G.coins;
+
 
 printf("\n");
 printf("Testing buying card worth less than 5....\n");
@@ -89,7 +108,7 @@ G.numActions = 1;
 int choice1 = 7;
 G.hand[currentPlayer][0] = feast;
 G.hand[currentPlayer][1] = feast;
-G.hand[currentPlayer][2] = 6;
+G.hand[currentPlayer][2] = gold;
 //G.handCount[currentPlayer] = 3;
 
 printf("\n");
@@ -117,10 +136,11 @@ for (int e = 0; e < G.handCount[currentPlayer]; e++) {
 
 
 assertMore(costs, newCost);
+*/
 
 printf("\n");
 printf("\n");
 printf("\n");
 
 return 0;
-};
+}
